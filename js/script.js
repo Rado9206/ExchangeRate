ed3550be3a886a632e937c0875324ccb19342923
Radek <rj.airoo@gmail.com>
2023-07-19 23:37:38 +0200

    added swap button

--- js/script.js
+++ js/script.js
@@ -9,19 +9,25 @@ const calculate = () => {
 	fetch(`https://api.exchangerate.host/latest?base=${currencyOne.value}&symbols=${currencyTwo.value}`)
 		.then(res => res.json())
 		.then(data => {
-
 			const currency1 = currencyOne.value
 			const currency2 = currencyTwo.value
 
-			const rate = data.rates[currency2];
-			rateInfo.textContent = `1 ${currency1} = ${rate.toFixed(2)} ${currency2}`;
+			const rate = data.rates[currency2]
+			rateInfo.textContent = `1 ${currency1} = ${rate.toFixed(2)} ${currency2}`
 			amountTwo.value = (amountOne.value * rate).toFixed(2)
 		})
+}
+
+const swap = () => {
+	const oldValue = currencyOne.value
+	currencyOne.value = currencyTwo.value
+	currencyTwo.value = oldValue
+	calculate()
 }
 
 currencyOne.addEventListener('change', calculate)
 currencyTwo.addEventListener('change', calculate)
 amountOne.addEventListener('input', calculate)
+swapBtn.addEventListener('click', swap)
 
 calculate()
-
